431c23d5df7cdd2021ee3cff3de520dc07afe3c4
Tamiz Rumey Jiffrey <z5592097@ad.unsw.edu.au>
2026-06-13 20:04:44 +1000

    basic file structure

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
-print("Hello from the new config!")
+-- print("Hello from the new config!")
 vim.g.mapleader = " "
+
+require('options')
+require('keymaps')
+require('autocmds')
+-- vim.o.syntax = true
---
+++ lua/keymaps.lua
@@ -0,0 +1,3 @@
+-- copy/paste to clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>p', '"+p')
