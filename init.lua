58c551fc2f4247d59b34546790b9d7df57ffad73
Tamiz RJ <z5592097@ad.unsw.edu.au>
2026-06-14 00:51:11 +1000

    update: colour scheme and new ui

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- print("Hello from the new config!")
 vim.g.mapleader = " "
 
+require('vim._core.ui2').enable()
 require('config.options')
 require('config.keymaps')
 require('config.autocmds')
--- vim.o.syntax = true
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
+
+-- linenos
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = 'a'
+
+-- splits
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.syntax = 'none'
+vim.cmd('colorscheme unokai')
+
