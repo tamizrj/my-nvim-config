89c52b852640fb4425c7500479387a20d8a9d08c
Tamiz RJ <z5592097@ad.unsw.edu.au>
2026-06-26 16:24:17 +1000

    common options

--- init.lua
+++ init.lua
@@ -14,16 +14,23 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- tabs
-vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4   -- Size of an indent (e.g. with >>)
-vim.opt.softtabstop = 4  -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.o.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 4   -- Size of an indent (e.g. with >>)
+vim.o.softtabstop = 4  -- Number of spaces that a <Tab> counts for while performing editing operations
 -- vim.opt.expandtab = true -- Convert tabs to spaces
+
+-- searching case sensitivity
+vim.o.ignorecase = true
+vim.o.smartcase = true  -- only case sensitive when caps included
 
 -- misc
 vim.cmd('colorscheme unokai')
 vim.o.mouse = 'a'
 vim.o.signcolumn = 'yes'
 vim.o.undofile = true
+vim.o.confirm = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 10
 
 -- whitespace characters
 vim.o.list = true
